36764c42a139ca594d94b5575e949155e32c4c16
zhenhua.zhu <865729986@qq.com>
2017-11-28 10:39:27 +0800

    基础数据表格

---
+++ Zzh.Backend/Controllers/DataGridController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zzh.Backend.Controllers
+{
+    public class DataGridController : Controller
+    {
+        // GET: DataGrid
+        public ActionResult BasicGrid()
+        {
+            return View();
+        }
+    }
+}
