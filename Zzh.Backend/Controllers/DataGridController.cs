5ba67469ba8b747e2128ae4b80dfd35d37a1c8d8
zhenhua.zhu <865729986@qq.com>
2017-12-01 11:03:07 +0800

    Grid基础绑定及属性设置

--- Zzh.Backend/Controllers/DataGridController.cs
+++ Zzh.Backend/Controllers/DataGridController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Zzh.Lib.DB.Repositorys;
 
 namespace Zzh.Backend.Controllers
 {
@@ -12,6 +14,15 @@ namespace Zzh.Backend.Controllers
         public ActionResult BasicGrid()
         {
             return View();
+        }
+
+        public async Task<JsonResult> GetList(int page, int rows,string asn_no)
+        {
+            using (EntAppAsnHeadRepository repo = new EntAppAsnHeadRepository())
+            {
+                var tuple = await repo.GetList(page, rows,asn_no);
+                return Json(new { total = tuple.Item1, rows = tuple.Item2 });
+            }
         }
     }
 }
--- Zzh.Lib/DB/Repositorys/EntAppAsnHeadRepository.cs
+++ Zzh.Lib/DB/Repositorys/EntAppAsnHeadRepository.cs
@@ -15,13 +15,14 @@ namespace Zzh.Lib.DB.Repositorys
         {
 
         }
-        public async Task<Tuple<int, List<EntAppAsnHead>>> GetList(int pageIndex, int pageSize)
+        public async Task<Tuple<int, List<EntAppAsnHead>>> GetList(int pageIndex, int pageSize, string asn_no)
         {
             try
             {
-                int total = await context.EntAppAsnHeads.CountAsync();
+                int total = await context.EntAppAsnHeads.Where(p => p.Asn_No.Contains(asn_no)).CountAsync();
                 int from = (pageIndex - 1) * pageSize;
                 var obj = await (from j in context.EntAppAsnHeads
+                                 where j.Asn_No.Contains(asn_no)
                                  orderby j.id
                                  select j).Skip(from).Take(pageSize).ToListAsync();
                 return Tuple.Create(total, obj);
