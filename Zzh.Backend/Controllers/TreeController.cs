e12acced1c12648c8dc05a1dbbadb1f9491bf951
zhenhua.zhu <865729986@qq.com>
2018-05-04 15:20:39 +0800

    Tree自定义

---
+++ Zzh.Backend/Controllers/TreeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zzh.Backend.Controllers
+{
+    public class TreeController : Controller
+    {
+        // GET: Tree
+        public ActionResult checker()
+        {
+            return View();
+        }
+    }
+}
