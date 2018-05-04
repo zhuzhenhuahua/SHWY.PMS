7e163b3258786194ba24517b7d077dfe1067a9c6
zhenhua.zhu <865729986@qq.com>
2018-05-04 18:08:12 +0800

    文件上传

---
+++ Zzh.Backend/Controllers/Demo/OperFileController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zzh.Backend.Controllers.Demo
+{
+    public class OperFileController : Controller
+    {
+        // GET: OperFile
+        public ActionResult UpDownload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            var fileName = file.FileName;
+            var filePath = Server.MapPath(string.Format("~/{0}", "Files"));
+            file.SaveAs(Path.Combine(filePath, fileName));
+            ViewBag.msg = "上传成功";
+            return View("UpDownload");
+        }
+    }
+}
