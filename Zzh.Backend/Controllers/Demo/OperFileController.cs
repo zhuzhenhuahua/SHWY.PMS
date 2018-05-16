66c0a31a6bf9f4a3a2eb808bdfc3e37464f3b68e
zhenhua.zhu <865729986@qq.com>
2018-05-16 17:03:07 +0800

    文件上传下载

--- Zzh.Backend/Controllers/Demo/OperFileController.cs
+++ Zzh.Backend/Controllers/Demo/OperFileController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Zzh.Backend.Models;
 
 namespace Zzh.Backend.Controllers.Demo
 {
@@ -12,16 +14,62 @@ namespace Zzh.Backend.Controllers.Demo
         // GET: OperFile
         public ActionResult UpDownload()
         {
-            return View();
+            string filePath = Server.MapPath(string.Format("~/{0}", "Files"));
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(filePath);
+            if (!directoryInfo.Exists)
+            {
+                Directory.CreateDirectory(filePath);
+            }
+            List<JTFileModel> fileModels = new List<JTFileModel>();
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                fileModels.Add(new JTFileModel()
+                {
+                    Name = file.Name,
+                    Extensioin = file.Extension,
+                    FullName = file.FullName,
+                    Length = file.Length,
+                    IsReadOnly = file.IsReadOnly,
+                    CreationTime = file.CreationTime
+                });
+            }
+            return View(new FileModesObj() { List = fileModels });
         }
         [HttpPost]
-        public ActionResult UploadFile(HttpPostedFileBase file)
+        public ActionResult UploadFile(HttpPostedFileBase filePara)
         {
-            var fileName = file.FileName;
             string filePath = Server.MapPath(string.Format("~/{0}", "Files"));
-            file.SaveAs(Path.Combine(filePath, fileName));
-            ViewBag.msg = "上传成功";
-            return View("UpDownload");
+            if (filePara != null && filePara.ContentLength > 0)
+            {
+                var fileName = filePara.FileName;
+                if (!Directory.Exists(filePath))
+                    Directory.CreateDirectory(filePath);
+                filePara.SaveAs(Path.Combine(filePath, fileName));
+                ViewBag.msg = "上传成功";
+            }
+          
+            List<JTFileModel> fileModels = new List<JTFileModel>();
+            DirectoryInfo directoryInfo = new DirectoryInfo(filePath);
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                fileModels.Add(new JTFileModel()
+                {
+                    Name = file.Name,
+                    Extensioin = file.Extension,
+                    FullName = file.FullName,
+                    Length = file.Length,
+                    IsReadOnly = file.IsReadOnly,
+                    CreationTime = file.CreationTime
+                });
+            }
+            return View("UpDownload", new FileModesObj() { List = fileModels });
+        }
+        public class FileModesObj
+        {
+            public List<JTFileModel> List { get; set; }
+            public int pageIndex { get; set; }
+            public int pageSize { get; set; }
         }
     }
 }
---
+++ Zzh.Backend/Models/JTFileModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zzh.Backend.Models
+{
+    public class JTFileModel
+    {
+        /// <summary>
+        /// 文件的名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 扩展名
+        /// </summary>
+        public string Extensioin { get; set; }
+        /// <summary>
+        /// 获取文件的全限定名称（完整路径）
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// 获取文件的大小（字节数）
+        /// </summary>
+        public long Length { get; set; }
+        /// <summary>
+        /// 是否只读
+        /// </summary>
+        public bool IsReadOnly { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+
+    }
+}
